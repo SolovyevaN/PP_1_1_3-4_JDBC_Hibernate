796ef97d82b4c01a5c63715073092e1a3ec4b1cb
Anastasia <nastya.solovyeva.99@gmail.com>
2024-12-28 19:17:56 +0300

    Исправила решение задачи: Main откорректирован

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -6,6 +6,20 @@ import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
+        Util.getConnection();
+        UserDao userDao = new UserDaoJDBCImpl();
+
+        userDao.createUsersTable();
+
+        userDao.saveUser("Petya", "Ivanov", (byte) 20);
+        userDao.saveUser("Zina", "Petrova", (byte) 25);
+        userDao.saveUser("Kolya", "Pupkin", (byte) 31);
+        userDao.saveUser("Max", "Sidorov", (byte) 38);
+
+        userDao.removeUserById(1);
+        userDao.getAllUsers();
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
 
     }
 }
